18ae7a669af311c4741e9f42f7768d59fd6fd045
kpwwalsh <wnuru@hotmail.com>
2018-11-08 17:32:38 -0600

    Update react to do P and S

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <ul>
+          { this.state.todos.map( (todo, index) => 
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            )}
+          </ul>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
