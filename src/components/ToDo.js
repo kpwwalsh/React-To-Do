9b92dd2089ff8469bf415421ec7df79d2196e1cb
kpwwalsh <wnuru@hotmail.com>
2018-11-14 20:10:24 -0600

    ToDo commit for event checkpoing

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,11 +4,11 @@ class ToDo extends Component {
   render() {
     return (
         <li>
-        <input type="checkbox" checked={ this.props.isCompleted } />
-        <span>{ this.props.description }</span>
+           <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+           <span>{ this.props.description }</span>
       </li>
     );
   }
 } 
 
-export default ToDo;
+export default ToDo;.
